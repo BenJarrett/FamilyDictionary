f4dcbb3eba7a89a4b382eddee4bbf83e2d0b1333
BenJarrett <b.cl.jarrett@gmail.com>
2021-08-07 08:44:40 -0500

    Not fully done, have error over my iteration of the dictionary

--- FamilyDictionary/Program.cs
+++ FamilyDictionary/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FamilyDictionary
 {
@@ -6,7 +7,21 @@ namespace FamilyDictionary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
+            myFamily.Add("sister", new Dictionary<string, string>() { { "name", "Krista" }, { "age", "42" } });
+            myFamily.Add("brother", new Dictionary<string, string>() { { "name", "Sam" }, { "age", "29" } });
+            myFamily.Add("brother", new Dictionary<string, string>() { { "name", "Matt" }, { "age", "31" } });
+            myFamily.Add("sister", new Dictionary<string, string>() { { "name", "Kaley" }, { "age", "36" } });
+            myFamily.Add("mom", new Dictionary<string, string>() { { "name", "Martha" }, { "age", "65" } });
+            myFamily.Add("dad", new Dictionary<string, string>() { { "name", "Terry" }, { "age", "65" } });
+
+            foreach (var item in myFamily)
+            {
+                Console.WriteLine($"{item.Value["name"]} is my {item.Key} and is {item.Value["age"]}");
+            }
+
         }
     }
 }
+
