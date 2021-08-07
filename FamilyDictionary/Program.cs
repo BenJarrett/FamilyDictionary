90ccc2aa3c281a4143b66bdd4865277d89ae0e96
BenJarrett <b.cl.jarrett@gmail.com>
2021-08-07 09:37:51 -0500

    finished iteration

--- FamilyDictionary/Program.cs
+++ FamilyDictionary/Program.cs
@@ -9,19 +9,19 @@ namespace FamilyDictionary
         {
 
             Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
-            myFamily.Add("sister", new Dictionary<string, string>() { { "name", "Krista" }, { "age", "42" } });
+            myFamily.Add("oldest sister", new Dictionary<string, string>() { { "name", "Krista" }, { "age", "42" } });
             myFamily.Add("brother", new Dictionary<string, string>() { { "name", "Sam" }, { "age", "29" } });
-            myFamily.Add("brother", new Dictionary<string, string>() { { "name", "Matt" }, { "age", "31" } });
-            myFamily.Add("sister", new Dictionary<string, string>() { { "name", "Kaley" }, { "age", "36" } });
+            myFamily.Add("oldest brother", new Dictionary<string, string>() { { "name", "Matt" }, { "age", "31" } });
+            myFamily.Add("older sister", new Dictionary<string, string>() { { "name", "Kaley" }, { "age", "36" } });
             myFamily.Add("mom", new Dictionary<string, string>() { { "name", "Martha" }, { "age", "65" } });
             myFamily.Add("dad", new Dictionary<string, string>() { { "name", "Terry" }, { "age", "65" } });
 
             foreach (var item in myFamily)
             {
+               
                 Console.WriteLine($"{item.Value["name"]} is my {item.Key} and is {item.Value["age"]}");
             }
 
         }
     }
 }
-
